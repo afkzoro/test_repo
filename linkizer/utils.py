0b32ffc38b8b1c9d0709396bc6877bb1f3a7916b
afkzoro <105337973+afkzoro@users.noreply.github.com>
2023-03-25 01:08:42 +0100

    bless

---
+++ linkizer/utils.py
@@ -0,0 +1,40 @@
+'''
+Utilities for Shortener.
+Facilitates the task of choosing random characters to create the code.
+'''
+from django.conf import settings
+
+from random import choice
+
+from string import ascii_letters, digits
+
+# Try to get the value from the settings module
+SIZE = getattr(settings, "MAXIMUM_URL_CHARS", 7)
+
+AVAIABLE_CHARS = ascii_letters + digits
+
+
+def create_random_code(chars=AVAIABLE_CHARS):
+    """
+    Creates a random string with the predetermined size
+    """
+    return "".join(
+        [choice(chars) for _ in range(SIZE)]
+    )
+
+
+def create_shortened_url(model_instance):
+    """The function takes as an argument a “Shortener” model instance.
+    Generates a random code using the create_random_code.
+    Gets the model class
+    Checks if there is any other object that has the same short_url"""
+    random_code = create_random_code()
+    # Gets the model class
+
+    model_class = model_instance.__class__
+
+    if model_class.objects.filter(short_url=random_code).exists():
+        # Run the function again
+        return create_shortened_url(model_instance)
+
+    return random_code
